c04f01cae61d6a1ecddfbf03960b42e085334ccc
JulianGomez <julian.gomez20@udea.edu.co>
2023-09-06 17:30:17 -0500

    Se agregan ejercicio 5 y 6

--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@ void mostrarmenu();
 void residuodivision();
 void ejercicio2();
 void ejercicio3();
+void ejercicio4();
+void ejercicio5();
+void ejercicio6();
 int main()
 {
-    unsigned int opcion=1;
-    while(opcion!=0){
+unsigned int opcion=1;
+while(opcion!=0){
 
 
 mostrarmenu();
@@ -24,9 +27,22 @@ cin>>opcion;
         ejercicio2();
 
      break;
+
      case 3:
         ejercicio3();
      break;
+
+    case 4:
+       ejercicio4();
+    break;
+
+    case 5:
+       ejercicio5();
+    break;
+
+    case 6:
+       ejercicio6();
+    break;
 
      default:
      if(opcion!=0)
@@ -167,4 +183,98 @@ void ejercicio3(){
         }
 
     }else cout<<"Mes invalido, ingrese otro valor para mes"<<endl;
+}
+
+void ejercicio4(){
+    int hora1= 0, hora2= 0, minutos1 = 0, minutos2 = 0;
+    float resultado = 0;
+    cout<<"Ingrese el valor para la hora 1 :  "<<endl;
+    cin>>hora1;
+    cout<<"Ingrese el valor para la hora 2 :  "<<endl;
+    cin>>hora2;
+
+//Hacer 1245%100 me da como residuo los minutos.
+
+    if(hora1/100<24 & hora1%100<60){
+        if(hora2/100<24 & hora2%100<60){
+        minutos1 = hora1%100;
+        hora1 = hora1/100;
+
+        minutos2 = hora2%100;
+        hora2 = hora2/100;
+
+        int horastotal = hora1+hora2, minutostotal=minutos1+minutos2;
+
+        if(minutostotal>=60){
+            horastotal++;
+            minutostotal = minutostotal-60;
+        }
+
+        if(horastotal>=24){
+            cout<<"Han pasado mas de 24 horas, es un nuevo día. "<<endl;
+            horastotal = horastotal-24;
+        }
+
+        cout<<horastotal;
+        cout<<minutostotal<<endl;
+
+
+
+        }
+        else
+        cout<<"Hora 2 invalida"<<endl;
+
+
+}
+else
+cout<<"Hora 1 invalida"<<endl;
+}
+
+void ejercicio5(){
+
+char asterisco = 42, espacio = ' ';
+cout<<"Ingrese un numero impar: ";
+int impar=0;
+cin>>impar;
+//Arriba
+for(int i = 1;i<=impar;i+=2){
+    int contador = (impar-i)/2;
+
+    for(int j = 0;j<contador;j++)
+    cout<<espacio;
+
+    for(int j=0;j<i;j++)
+    cout<<asterisco;
+
+    cout<<endl;
+}
+
+//Abajo
+for(int i = impar -2;i>=1;i-=2){
+    int contador = (impar-i)/2;
+
+    for(int j = 0;j<contador;j++)
+    cout<<espacio;
+
+    for(int j=0;j<i;j++)
+    cout<<asterisco;
+
+    cout<<endl;
+}
+}
+
+void ejercicio6(){
+float euler, aux, valoriteracion, i= 1;
+cout<<"Ingrese el numero de iteraciones para euler: ";
+cin>>euler;
+while(i!=euler){
+    float iterador = i;
+    valoriteracion = 1/iterador;
+    aux = aux + valoriteracion;
+    cout<<"Iteracion = "<<aux<<endl;//prueba para el ciclo for
+
+    i++;
+}
+cout<<"Iteracion = "<<aux+1<<endl;
+cout<<"El valor de euler en este caso es aproximadamente = "<<aux+1<<endl;
 }
